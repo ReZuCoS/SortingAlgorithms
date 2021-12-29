b429560bf6be59c978c48e323511e34b31dd082d
Василий Цыганок <tsyganok.vasya@yandex.ru>
2021-12-29 03:17:21 +0300

    Add project files.

---
+++ SortingAlgorithms/Data/DataGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SortingAlgorithms.Data
+{
+    partial class DataGenerator
+    {
+        public static int[] GenerateArrayData(int arrayDimension)
+        {
+            int[] array = new int[arrayDimension];
+
+            for (int i = 0; i < arrayDimension; i++)
+            {
+                array[i] = i + 1;
+            }
+
+            return array;
+        }
+
+        public static int[] GenerateRandomArrayData(int arrayDimension)
+        {
+            int[] array = new int[arrayDimension];
+
+            Random random = new();
+
+            for (int i = 0; i < arrayDimension; i++)
+            {
+                array[i] = random.Next(-1000, 1000);
+            }
+
+            return array;
+        }
+    }
+}
---
+++ SortingAlgorithms/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using SortingAlgorithms.Data;
+
+namespace SortingAlgorithms
+{
+    partial class Program
+    {
+        static void Main()
+        {
+            int[] array100 = DataGenerator.GenerateArrayData(100);
+            int[] array100R = DataGenerator.GenerateRandomArrayData(100);
+
+            List<int[]> list = new List<int[]>();
+
+            list.Add(array100);
+            list.Add(array100R);
+
+            Console.ReadKey();
+        }
+    }
+}
